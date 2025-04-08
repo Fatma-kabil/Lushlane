8d99b68fc2738c971a123881a75f24b7c2526adc
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-08 02:19:45 +0200

    integrate resset password cubit

--- lib/features/auth/presentation/views/reset_password_view.dart
+++ lib/features/auth/presentation/views/reset_password_view.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lushlane_app/constants.dart';
+import 'package:lushlane_app/core/show_snack_bar.dart';
+import 'package:lushlane_app/core/utils/loading_indecator.dart';
+import 'package:lushlane_app/features/auth/presentation/manger/reset_password_cubit/reset_password_cubit.dart';
 import 'package:lushlane_app/features/auth/presentation/views/widgets/resset_password_view_body.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: RessetPasswordViewBody(),
-      ),
+    return BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
+      listener: (context, state) {
+        
+        if (state is ResetPasswordLoading) {
+        } else if (state is ResetPasswordSuccess) {
+          showSnakBar(context, 'Check your email to reset your password!');
+        } else if (state is ResetPasswordFailure) {
+          showSnakBar(context, state.errMassege);
+        }
+      },
+      builder: (context, state) {
+        bool isLoading = state is ResetPasswordLoading;
+
+        return ModalProgressHUD(
+          progressIndicator: LoadingIndecator(),
+          inAsyncCall: isLoading,
+          child: SafeArea(
+            child: Scaffold(
+              backgroundColor: kPrimaryColor,
+              body: RessetPasswordViewBody(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
