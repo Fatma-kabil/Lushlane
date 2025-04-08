ae176918de004dbfde517ec8c627e1ddd77020e5
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-08 02:01:26 +0200

    create function for resset password cubit

--- lib/features/auth/presentation/manger/reset_password_cubit/reset_password_cubit.dart
+++ lib/features/auth/presentation/manger/reset_password_cubit/reset_password_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'reset_password_state.dart';
 
 class ResetPasswordCubit extends Cubit<ResetPasswordState> {
   ResetPasswordCubit() : super(ResetPasswordInitial());
+
+   Future<void> resetPassword({required String email}) async {
+    emit(ResetPasswordLoading());
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      emit(ResetPasswordSuccess());
+    } on FirebaseAuthException catch (e) {
+      emit(ResetPasswordFailure(errMassege: e.message ?? 'Something went wrong'));
+    }
+  }
 }
--- lib/features/auth/presentation/manger/reset_password_cubit/reset_password_state.dart
+++ lib/features/auth/presentation/manger/reset_password_cubit/reset_password_state.dart
@@ -4,3 +4,13 @@ part of 'reset_password_cubit.dart';
 sealed class ResetPasswordState {}
 
 final class ResetPasswordInitial extends ResetPasswordState {}
+
+class ResetPasswordSuccess extends ResetPasswordState {}
+
+class ResetPasswordLoading extends ResetPasswordState {}
+
+// ignore: must_be_immutable
+class ResetPasswordFailure extends ResetPasswordState {
+  String errMassege;
+  ResetPasswordFailure({required this.errMassege});
+}
