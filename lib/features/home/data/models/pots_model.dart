81b94b4ad8674e2d845974bc07dd9202330981ee
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-10 10:34:30 +0200

    create home repo methods

--- lib/features/home/data/models/pots_model.dart
+++ lib/features/home/data/models/pots_model.dart
@@ -1,4 +1,4 @@
-class Pot {
+class PotsModel {
   final String name;
   final String description;
   final double price;
@@ -7,7 +7,7 @@ class Pot {
   final String location;
   final String image;
 
-  Pot({
+  PotsModel({
     required this.name,
     required this.description,
     required this.price,
@@ -17,8 +17,8 @@ class Pot {
     required this.image,
   });
 
-  factory Pot.fromJson(Map<String, dynamic> json) {
-    return Pot(
+  factory PotsModel.fromJson(Map<String, dynamic> json) {
+    return PotsModel(
       name: json['name'],
       description: json['description'],
       price: json['price'].toDouble(),
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:lushlane_app/core/errors/failures.dart';
+import 'package:lushlane_app/features/home/data/models/pots_model.dart';
+
 abstract class HomeRepo {
-  fetchpots();
+Future<Either<Failure,List<PotsModel>>>  fetchpots();
 }
