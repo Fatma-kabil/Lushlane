699b9ab7daa64f4ceb70cf976a610b2183e28dba
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-10 11:31:03 +0200

    create fetch pots method

--- lib/features/home/presentation/manager/fetch_pots_cubit/fetch_pots_cubit.dart
+++ lib/features/home/presentation/manager/fetch_pots_cubit/fetch_pots_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:lushlane_app/features/home/data/models/pots_model.dart';
+import 'package:lushlane_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_pots_state.dart';
 
 class FetchPotsCubit extends Cubit<FetchPotsState> {
-  FetchPotsCubit() : super(FetchPotsInitial());
+  FetchPotsCubit(this.homerepo) : super(FetchPotsInitial());
+  final HomeRepo homerepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FetchPotsLoading());
+    var result = await homerepo.fetchpots();
+
+    result.fold(
+      (failure) {
+        emit(FetchPotsFailure(errMessage: failure.errorMessage));
+      },
+      (pots) {
+        emit(FetchPotsSucsses(pots: pots));
+      },
+    );
+  }
 }
