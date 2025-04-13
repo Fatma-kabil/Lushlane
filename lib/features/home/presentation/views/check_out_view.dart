fe22e2b16c4e926e311d84dc273845b1123c74c2
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-14 01:31:15 +0200

    refactor code

--- lib/features/home/presentation/views/check_out_view.dart
+++ lib/features/home/presentation/views/check_out_view.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lushlane_app/features/home/data/models/pots_model.dart';
+import 'package:lushlane_app/features/home/presentation/manager/fetch_pots_cubit/cart_cubit.dart/cart_cubit.dart';
 import 'package:lushlane_app/features/home/presentation/views/widgets/check_out_view_body.dart';
 import 'package:lushlane_app/features/home/presentation/views/widgets/custom_appbar.dart';
 
 class CheckOutView extends StatelessWidget {
-  const CheckOutView({super.key, });
-  
+  const CheckOutView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CustomAppBar(title: 'Check out'),
-      body: CheckOutViewBody(),
+      body: BlocBuilder<CartCubit, Map<String, dynamic>>(
+        builder: (context, cartState) {
+          List<PotsModel> cartItems = List<PotsModel>.from(cartState['items']);
+
+          double totalPrice = cartItems.fold(
+            0.0,
+            (sum, item) => sum + item.price,
+          );
+
+          return CheckOutViewBody(
+            cartItems: cartItems,
+            totalPrice: totalPrice,
+          );
+        },
+      ),
     );
   }
 }
+
