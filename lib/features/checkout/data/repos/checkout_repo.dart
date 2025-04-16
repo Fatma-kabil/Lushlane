8e0c5c8976fe141276a13f7ba0f56bae6d51e5eb
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-16 10:58:02 +0200

    create chekout repo

---
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:lushlane_app/core/errors/failures.dart';
+import 'package:lushlane_app/features/checkout/data/models/payment_intent_model.dart';
+ 
+ 
+ abstract class CheckoutRepo {
+   Future<Either<Failure, void>> makePayment(
+       {required PaymentIntentInputModel paymentIntentInputModel});
+ }
