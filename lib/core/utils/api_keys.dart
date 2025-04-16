72e75df7c1ad49bdf9dabdda5ff70cdb8e3acf30
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-16 10:41:20 +0200

    implement payment intent

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,5 @@
+class ApiKeys {
+  static String secretKey =
+      'sk_test_51R2LxrCoYUgtrkAm2lmHkmDDYyEeflAsPJBTuJCpRqDh8TRvhLlzmeUTustAPMXm8BfV0umszrqPOm3A4wiFzUC800JAYNVdit';
+  static String publishKey = 'pk_test_51R2LxrCoYUgtrkAmVvNx1DE6oIsXaDrC4vDAoJmPFNXUTNcHBq4XoOA9O3zshzWIiYh4NR1J6KJG4JmTR0OZ84gP00sJZ0rrCw';
+}
--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,5 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:lushlane_app/core/utils/api_keys.dart';
+import 'package:lushlane_app/core/utils/api_service.dart';
+import 'package:lushlane_app/features/checkout/data/models/payment_intent_model.dart';
+import 'package:lushlane_app/features/checkout/data/models/payment_intent_models/payment_intent_models.dart';
+
 class StripeService {
+
 // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel)
-
-
+final ApiService apiservice = ApiService(dio: Dio());
+   Future<PaymentIntentModels> createPaymentIntent(
+       PaymentIntentInputModel paymentIntentInputModel) async {
+     var response = await apiservice.post(
+         body: paymentIntentInputModel.toJson(),
+         url: 'https://api.stripe.com/v1/payment_intents',
+         token: ApiKeys.secretKey);
+     var paymentIntentModel = PaymentIntentModels.fromJson(response.data);
+     return paymentIntentModel;
+   }
 }
+
