3aa3f197b64df2b4951cc1b67695b4634c54b232
Fatma_kabil <fatmakhaled112@gmail.com>
2025-04-29 08:51:35 +0200

    dynamic name & email

--- lib/features/drawer/presentation/views/widgets/drawer_view_body.dart
+++ lib/features/drawer/presentation/views/widgets/drawer_view_body.dart
@@ -1,68 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lushlane_app/constants.dart';
-import 'package:lushlane_app/features/drawer/presentation/manager/profile_cubit/profile_cubit.dart';
-import 'package:lushlane_app/features/drawer/presentation/views/widgets/profile_image_bloc_builder.dart';
+import 'package:lushlane_app/features/drawer/presentation/views/widgets/header.dart';
 
 class DrawerViewBody extends StatelessWidget {
   const DrawerViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return  Drawer(
+    final user = FirebaseAuth.instance.currentUser;
+    final username = user?.displayName ?? 'Guest';
+    final email = user?.email ?? 'guest@example.com';
+
+    return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    // استخدام ويدجت ProfileImageWidget هنا
-                    const ProfileImageBlocBuilder(),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: GestureDetector(
-                        onTap: () {
-                          context.read<ProfileCubit>().pickImage();
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          padding: const EdgeInsets.all(4),
-                          child: Icon(
-                            Icons.add,
-                            size: 20,
-                            color: kPrimaryColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "John Doe", // دينامك بعدين لو عايزه
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Text(
-                  "john.doe@example.com", // دينامك بعدين لو عايزه
-                  style: TextStyle(color: Colors.white70, fontSize: 14),
-                ),
-              ],
-            ),
-          ),
+          Header(username: username, email: email),
           ListTile(
             leading: const Icon(Icons.home),
             title: const Text('Home'),
---
+++ lib/features/drawer/presentation/views/widgets/header.dart
@@ -0,0 +1,73 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lushlane_app/constants.dart';
+import 'package:lushlane_app/features/drawer/presentation/manager/profile_cubit/profile_cubit.dart';
+import 'package:lushlane_app/features/drawer/presentation/views/widgets/profile_image_bloc_builder.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    super.key,
+    required this.username,
+    required this.email,
+  });
+
+  final String username;
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: kPrimaryColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              const ProfileImageBlocBuilder(),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () {
+                    context.read<ProfileCubit>().pickImage();
+                  },
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    padding: const EdgeInsets.all(4),
+                    child: Icon(
+                      Icons.add,
+                      size: 20,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            username,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            email,
+            style: const TextStyle(
+              color: Colors.white70,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
